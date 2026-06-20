e13ac231883bf58e51390a71fd66aa6aeefff7bd
22UEE108 <118985392+22UEE108@users.noreply.github.com>
2026-06-21 02:51:19 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+
 from models import (
     TripInput,
+    UpdateTrip,
     PredictionResult
 )
 
-from battery import predict_soc
+
+from battery import calculate_prediction
 
 
 
@@ -19,11 +22,14 @@ app = FastAPI(
 def home():
 
     return {
-        "status":
+        "message":
         "EV Range Optimizer Running"
     }
 
 
+
+
+# Before journey
 
 @app.post(
     "/predict",
@@ -33,11 +39,33 @@ def predict(
     trip: TripInput
 ):
 
-    result = predict_soc(
+    result = calculate_prediction(
         trip.soc,
         trip.distance,
         trip.traffic
     )
 
+    return result
+
+
+
+
+# During journey updates
+
+@app.post(
+    "/update-trip",
+    response_model=PredictionResult
+)
+def update_trip(
+    update: UpdateTrip
+):
+
+    result = calculate_prediction(
+        update.current_soc,
+        update.remaining_distance,
+        update.traffic
+    )
+
 
     return result
+
