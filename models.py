d8e1a8c3c92e084d0fed36c01e75ef058cbff716
22UEE108 <118985392+22UEE108@users.noreply.github.com>
2026-06-21 02:52:04 +0530

    models.py

--- models.py
+++ models.py
@@ -1,13 +1,25 @@
 from pydantic import BaseModel
 
 
+
 class TripInput(BaseModel):
+
     soc: float
     distance: float
     traffic: str
 
 
+
+class UpdateTrip(BaseModel):
+
+    current_soc: float
+    remaining_distance: float
+    traffic: str
+
+
+
 class PredictionResult(BaseModel):
+
     predicted_soc: float
     charging_required: bool
     message: str
